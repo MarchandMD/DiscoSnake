a4121395bf6a64fdacd941d592562cd364089997
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-04-13 11:46:54 -0600

    changes

--- Snake-JavaScript-master/snake.js
+++ Snake-JavaScript-master/snake.js
@@ -29,6 +29,15 @@ foodImg.src = "img/food.png";
 // right.src = "audio/right.mp3";
 // down.src = "audio/down.mp3";
 
+//create snake
+//this happens once; Because it is outside of the draw() function. After the draw() function draws the snake once....the snake here is dissapearing. I have to figure out why it's dissapearing. My guess is that because I do snake.pop() just randomly. but what does .pop() return when I call it? it returns the value that was popped off.....so it's the last thing
+let snake = [];
+snake[0] = {
+    x: 9 * box,
+    y: 10 * box
+}
+
+
 
 
 
@@ -41,40 +50,26 @@ let food = {
 //create the score variable
 let score = 0;
 
-//create snake
-let snake = [];
-snake[0] = {
-    x: 9 * box,
-    y: 10 * box
-}
-
-//old head position
-let snakeX = snake[0].x;
-let snakeY = snake[0].y;
-console.log(snakeX);
 
 //CONTROL THE SNAKE
 //with keys; 
 let d;
 
-
 document.addEventListener("keydown", direction);
 
-function direction(e) {
-    if (e.keyCode == 37 && d != "RIGHT") {
-        //left.play();
+function direction(event) {
+    if (event.keyCode == 37 && d != "RIGHT") {
         d = "LEFT";
         console.log(d);
         return d;
-    } else if (e.keyCode == 38 && d != "DOWN") {
-        //up.play();
+    } else if (event.keyCode == 38 && d != "DOWN") {
         d = "UP";
         console.log(d);
-    } else if (e.keyCode == 39 && d != "LEFT") {
+    } else if (event.keyCode == 39 && d != "LEFT") {
         //right.play();
         d = "RIGHT";
         console.log(d);
-    } else if (e.keyCode == 40 && d != "UP") {
+    } else if (event.keyCode == 40 && d != "UP") {
         //down.play();
         d = "DOWN";
         console.log(d);
@@ -85,23 +80,27 @@ function direction(e) {
 //How to draw Snake && ground && food && score
 //draw everything to the canvas
 function draw() {
-
-
-
     ctx.drawImage(ground, 0, 0); //draws the ground image at 0,0 of the canvas
 
+    //draws the snake
     for (let i = 0; i < snake.length; i++) {
         ctx.fillStyle = (i == 0) ? "green" : "white";
-
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
-
         ctx.strokeStyle = "red";
         ctx.strokeRect(snake[i].x, snake[i].y, box, box);
-    }
+    };
 
     ctx.drawImage(foodImg, food.x, food.y);
 
+    //old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
 
+
+    //remove the tail...so he's trying to remove the tail...but if there is no tail...it just removes the entire snake....so I feel like I need to say, if snake.length > 1
+    if (snake.length >= 1) {
+        snake.pop();
+    }
 
     //which direction
     if (d == "LEFT") snakeX -= box;
@@ -109,11 +108,7 @@ function draw() {
     if (d == "RIGHT") snakeX += box;
     if (d == "DOWN") snakeY += box;
 
-
-    //remove the tail
-    //snake.pop();
-
-    //ADD NEW HEAD
+    // ADD NEW HEAD
     let newHead = {
         x: snakeX,
         y: snakeY
@@ -121,39 +116,40 @@ function draw() {
 
     snake.unshift(newHead);
 
-
+    //drawing the score
     ctx.fillStyle = "white";
     ctx.font = "45px Arial";
     ctx.fillText(score, 2 * box, 1.6 * box);
 
     //if snake eats the food
+    //Then i want to push the coordinates to snake; so I'd want to snake.push(foodCoordinates);
+
     if (snakeX == food.x && snakeY == food.y) {
         score++;
-        //eat.play();
+        snake.push(newHead);
         food = {
-            x: unit * Math.floor(Math.random() * 17 + 1),
-            y: unit * Math.floor(Math.random() * 15 + 3)
-        }
+            x: box * Math.floor(Math.random() * 17 + 1),
+            y: box * Math.floor(Math.random() * 15 + 3)
+        };
     } else {
-        snake.pop();
+        //    snake.pop();
     }
 
 
-    if (snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead, snake)) {
+    if (snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box /* || collision(newHead, snake) */) {
         clearInterval(game);
-        //dead.play();
+        console.log(newHead);
     }
 
     function collision(head, array) {
         for (let i = 0; i < array.length; i++) {
             if (head.x == array[i].x && head.y == array[i].y) {
-                return true;
+                console.log('same');
             }
         }
-        return false;
     }
 
 }
 
 //call the draw function every 100ms
-let game = setInterval(draw, 100); //calls the draw function every 100ms 
+//let game = setInterval(draw, 200); //calls the draw function every 100ms 
